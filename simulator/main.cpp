62d97e09cb8088292d6962524660275c7e20d895
Shalev <shalev.druk@walla.com>
2020-05-30 12:14:07 +0300

    Add num_of_threads input support

--- simulator/main.cpp
+++ simulator/main.cpp
@@ -2,18 +2,20 @@
 #include "Simulator.h"
 
 enum PathType {
-    Travel, Algo, Output, None
+    Travel, Algo, Output, NumThreads, None
 };
 
 PathType getTypeOfPath(const string &input) {
     if (input == "-travel_path") return Travel;
     if (input == "-algorithm_path") return Algo;
     if (input == "-output") return Output;
+    if (input == "-num_threads") return NumThreads;
     return None;
 
 }
 
-bool initializeParameters(string &travel_path, string &algorithm_path, string &output_path, int num_of_params,
+bool initializeParameters(string &travel_path, string &algorithm_path, string &output_path, int &num_of_threads,
+                          int num_of_params,
                           char *argv[]) {
     if (num_of_params < 2 || num_of_params % 2 == 0) {
         cout << "@ FATAL ERROR: Wrong number of arguments was given." << endl;
@@ -37,13 +39,21 @@ bool initializeParameters(string &travel_path, string &algorithm_path, string &o
                 output_path = argv[i + 1]; // i+1 must exists because of the way we iterate through the words
                 break;
             }
+            case NumThreads: {
+                if (!isPositiveNumber(argv[i + 1])) {
+                    cout << "@ ERROR: Number of threads given is invalid, setting the number to 1." << endl;
+                    num_of_threads = 1;
+                } else
+                    num_of_threads = string2int(argv[i + 1]);
+                break;
+            }
             case None: {
                 cout << "@ FATAL ERROR: Invalid parameters was given." << endl;
                 return false;
             }
         }
     }
-    if(travel_path.empty()){
+    if (travel_path.empty()) {
         cout << "@ FATAL ERROR: No travel_path was given." << endl;
         return false; // return false if there was not -travel_path param
     }
@@ -54,20 +64,21 @@ int main(int argc, char *argv[]) {
     string travel_path = "";
     string algorithm_path = "";
     string output_path = "";
+    int num_of_threads = 1;
     bool clean_run;
-    if (argc > 7) {
+    if (argc > 9) {
         cout << "@ FATAL ERROR: Too many arguments given." << endl;
         return EXIT_FAILURE;
     }
-    if (!initializeParameters(travel_path, algorithm_path, output_path, argc, argv)) {
+    if (!initializeParameters(travel_path, algorithm_path, output_path, num_of_threads, argc, argv)) {
         // README: if any flag is declared and the path given is empty, an error will be printed and the simulation will not start.
         return EXIT_FAILURE;
     }
-
+    cout << "Threads given:" << num_of_threads << endl;
     Simulator sim(output_path);
     clean_run = sim.runSimulation(algorithm_path, travel_path);
     sim.printSimulationErrors();
-    if(clean_run){
+    if (clean_run) {
         sim.printSimulationResults();
     }
     return EXIT_SUCCESS;
